97a74cc6e9df2df1a318651fd26db3da32fc1cbd
oneight2 <1606109@sttgarut.ac.id>
2020-05-01 21:42:43 +0700

    Updates

---
+++ precache-manifest.71f1da317348e8813b8381f8bd17cc97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9246eca44bf47e047608320fa5d44582",
+    "url": "/Belajar-SPA-React/index.html"
+  },
+  {
+    "revision": "c4ca5172e29d4e5c2271",
+    "url": "/Belajar-SPA-React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "70652a5e89273b89053f",
+    "url": "/Belajar-SPA-React/static/js/2.49cf159e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Belajar-SPA-React/static/js/2.49cf159e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ca5172e29d4e5c2271",
+    "url": "/Belajar-SPA-React/static/js/main.841afe76.chunk.js"
+  },
+  {
+    "revision": "c33dbbc2309ad38bd87d",
+    "url": "/Belajar-SPA-React/static/js/runtime-main.e5435e2e.js"
+  }
+]);
